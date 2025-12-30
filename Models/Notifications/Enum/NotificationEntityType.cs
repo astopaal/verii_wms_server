5e61c951134a9f18ded524ead4bcdf401d3c14de
cannasif-ship-it <can.nasif@v3rii.com>
2025-12-30 06:04:56 -0800

    feat: Add WTDone entity type constant
    - Add WTDone constant to NotificationEntityType for completed warehouse transfers

---
+++ Models/Notifications/Enum/NotificationEntityType.cs
@@ -0,0 +1,23 @@
+namespace WMS_WEBAPI.Models
+{
+    /// <summary>
+    /// Notification related entity type constants
+    /// Used in RelatedEntityType field to identify which module/entity the notification is about
+    /// </summary>
+    public static class NotificationEntityType
+    {
+        public const string PHeader = "PHeader";
+        public const string GRHeader = "GRHeader";
+        public const string WTHeader = "WTHeader";
+        public const string WTDone = "WTDone";
+        public const string PRHeader = "PRHeader";
+        public const string PTHeader = "PTHeader";
+        public const string SITHeader = "SITHeader";
+        public const string SRTHeader = "SRTHeader";
+        public const string WIHeader = "WIHeader";
+        public const string WOHeader = "WOHeader";
+        public const string SHHeader = "SHHeader";
+        public const string ICHeader = "ICHeader";
+    }
+}
+
